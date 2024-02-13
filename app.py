a889182afed8987fe9fe36cb8e184cf1432a22ef
Muhammad Atif Ali <me@matifali.dev>
2024-02-14 01:15:37 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_env_variables():
+    # List of environment variables to display
+    vars_to_display = ['SECRET_1', 'SECRET_2']
+    env_vars = '<br>'.join([f'{var}: {os.environ.get(var, "Not Found")}' for var in vars_to_display])
+    return f'<h1>Selected Environment Variables</h1><p>{env_vars}</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ demo_12.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Get the secrets from vault
+SECRET_1=$(vault kv get -format="json" "coder/sample-app" | jq -r '.data.data.SECRET_1')
+SECRET_2=$(vault kv get -format="json" "coder/sample-app" | jq -r '.data.data.SECRET_2')
+
+# Set the secrets in the environment
+export SECRET_1=$SECRET_1
+export SECRET_2=$SECRET_2
+
+# Start the app
+python3 app.py
